ed3ee2d41ab1c5c1d91adc78f70300442d9f8be1
Lê Thanh Bình <binh@sunrise.vn>
2015-11-03 22:08:00 +0700

    Notification

--- Core/Push/NotificationPusher.php
+++ Core/Push/NotificationPusher.php
@@ -7,9 +7,11 @@ use AppBundle\Entity\Organisation\Organisation;
 use AppBundle\Services\Core\Framework\BaseController;
 use AppBundle\Entity\Core\Message\Message;
 
-class NotificationPusher extends BaseController {
+class NotificationPusher extends BaseController
+{
 
-    public function push(Organisation $organisation, Message $message, Push $push, $current) {
+    public function push(Organisation $organisation, Message $message, Push $push, $current)
+    {
         //get paging
         $newCurrent = $push->getCurrent();
         $size = $push->getSize();
@@ -18,30 +20,31 @@ class NotificationPusher extends BaseController {
             //get positions
             $em = $this->getContainer()->get('doctrine')->getManager();
             $queryBuilder = $em->createQueryBuilder()
-                    ->select('position')
-                    ->from('AppBundle:Organisation\Position', 'position')
-                    ->join('position.employee', 'employee')
-                    ->join('position.employer', 'employer', 'WITH', 'employer = ?1')->setParameter(1, $organisation)
-                    ->andWhere('position.active = ?2')->setParameter(2, true)
-                    ->andWhere('position.createdAt <= ?3')->setParameter(3, $message->getCreatedAt())
-                    ->setFirstResult($current * $size)
-                    ->setMaxResults(($newCurrent - $current) * $size);
+                ->select('position')
+                ->from('AppBundle:Organisation\Position', 'position')
+                ->join('position.employee', 'employee')
+                ->join('position.employer', 'employer', 'WITH', 'employer = ?1')->setParameter(1, $organisation)
+                ->andWhere('position.active = ?2')->setParameter(2, true)
+                ->andWhere('position.createdAt <= ?3')->setParameter(3, $message->getCreatedAt())
+                ->setFirstResult($current * $size)
+                ->setMaxResults(($newCurrent - $current) * $size);
             $positions = $queryBuilder->getQuery()->getResult();
             //set message
-            $tag = $this->getContainer()->get('doctrine')
-                    ->getRepository('AppBundle:Core\Core\Tag')
-                    ->findByName(Message::TAG_NOTIFICATION);
+            $tag = $this->getDoctrine()
+                ->getRepository('AppBundle:Core\Core\Tag')
+                ->findOneBy(array('name' => Message::TAG_NOTIFICATION));
             foreach ($positions as $position) {
                 $messageUser = new Message();
                 $messageUser->setRecipient($position->getEmployee());
                 $messageUser->setSubject($message->getSubject());
                 $messageUser->setBody($message->getBody());
+                $messageUser->setParent($message);
                 if ($tag) {
                     $messageUser->addTag($tag);
                 }
                 $em->persist($messageUser);
-                $em->flush();
             }
+            $em->flush();
             //update push
             $push->setCurrent($newCurrent);
             $em->persist($push);
