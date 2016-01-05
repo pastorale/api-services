93c0184ae229b8a175817d002ee87ce904892d4c
Lê Thanh Bình <binh@sunrise.vn>
2016-01-06 01:00:54 +0700

    Guzzle Bundle

--- Core/Push/NotificationPusher.php
+++ Core/Push/NotificationPusher.php
@@ -7,9 +7,11 @@ use AppBundle\Entity\Organisation\Organisation;
 use AppBundle\Services\Core\Framework\BaseController;
 use AppBundle\Entity\Core\Message\Message;
 
-class NotificationPusher extends BaseController {
+class NotificationPusher extends BaseController
+{
 
-    public function push(Organisation $organisation, Message $message, Push $push, $current) {
+    public function push(Organisation $organisation, Message $message, Push $push, $current)
+    {
         //get paging
         $newCurrent = $push->getCurrent();
         $size = $push->getSize();
@@ -20,19 +22,19 @@ class NotificationPusher extends BaseController {
 
             // todo: move this into its own repo
             $queryBuilder = $em->createQueryBuilder()
-                    ->select('position')
-                    ->from('AppBundle:Organisation\Position', 'position')
-                    ->join('position.employee', 'employee')
-                    ->join('position.employer', 'employer', 'WITH', 'employer = ?1')->setParameter(1, $organisation)
-                    ->andWhere('position.active = ?2')->setParameter(2, true)
-                    ->andWhere('position.createdAt <= ?3')->setParameter(3, $message->getCreatedAt())
-                    ->setFirstResult($current * $size)
-                    ->setMaxResults(($newCurrent - $current) * $size);
+                ->select('position')
+                ->from('AppBundle:Organisation\Position', 'position')
+                ->join('position.employee', 'employee')
+                ->join('position.employer', 'employer', 'WITH', 'employer = ?1')->setParameter(1, $organisation)
+                ->andWhere('position.active = ?2')->setParameter(2, true)
+                ->andWhere('position.createdAt <= ?3')->setParameter(3, $message->getCreatedAt())
+                ->setFirstResult($current * $size)
+                ->setMaxResults(($newCurrent - $current) * $size);
             $positions = $queryBuilder->getQuery()->getResult();
             //set message
             $tag = $this->getDoctrine()
-                    ->getRepository('AppBundle:Core\Classification\Tag')
-                    ->findOneBy(array('name' => Message::TAG_NOTIFICATION));
+                ->getRepository('AppBundle:Core\Classification\Tag')
+                ->findOneBy(array('name' => Message::TAG_NOTIFICATION));
             foreach ($positions as $position) {
                 //save notifycation to db
                 $messageUser = new Message();
@@ -57,7 +59,7 @@ class NotificationPusher extends BaseController {
                 foreach ($userDevices as $userDevice) {
                     $registrationIds[] = $userDevice->getDeviceToken();
                 }
-                if (count($registrationIds)) {
+                if (count($registrationIds) > 0) {
                     $client->send($data, $registrationIds);
                 }
                 //end send
