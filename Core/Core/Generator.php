5bb39ea75dc577913197133630be64ee84462210
Lê Thanh Bình <binh@sunrise.vn>
2016-01-27 18:15:39 +0700

    Process Media

---
+++ Core/Core/Generator.php
@@ -0,0 +1,17 @@
+<?php
+namespace AppBundle\Services\Core\Core;
+
+class Generator
+{
+    function generateRandomString($length = 12)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+}
