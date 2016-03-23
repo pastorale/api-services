104e19a97f7cda4b34898c5941d8d3dc58903665
Lê Thanh Bình <binh@sunrise.vn>
2016-03-23 22:58:12 +0700

    - install KNPMenuBundle - perhaps we need this in the future -     return $this->fetchByOwner('AppBundle:Organisation\Handbook\Handbook', 'handbook', 'organisation', $organisationId, ['get_organisation_handbooks', array(               'organisationId' => $organisationId)]);

--- Core/Framework/ControllerService.php
+++ Core/Framework/ControllerService.php
@@ -4,6 +4,7 @@ namespace AppBundle\Services\Core\Framework;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ControllerService extends BaseController implements ClassResourceInterface
@@ -17,5 +18,24 @@ class ControllerService extends BaseController implements ClassResourceInterface
         return $response;
     }
 
+    /**
+     * @param string $owned
+     * @param string $alias
+     * @param string $owner
+     * @param int $id
+     * @param array $routeArray
+     * @return mixed
+     */
+    public function fetchByOwner($owned, $alias, $owner, $id, $routeArray)
+    {
+        $request = $this->getRequest();
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->createQueryBuilder()
+            ->select($alias)
+            ->from($owned, $alias)
+            ->where($alias . '.' . $owner . '= ?1')
+            ->setParameter(1, $id);
+        return ($this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], false));
+    }
 
 }
