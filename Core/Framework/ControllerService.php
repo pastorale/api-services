22d69a232ffb2b07fd06a3e01207fbfeefe2530b
Lê Thanh Bình <binh@sunrise.vn>
2016-03-28 23:31:59 +0700

    - implement OwnableInterface for JobListing - fetchByOwningSide improvement

--- Core/Framework/ControllerService.php
+++ Core/Framework/ControllerService.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Services\Core\Framework;
 
+use Doctrine\ORM\QueryBuilder;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 
 use FOS\RestBundle\Controller\FOSRestController;
@@ -18,22 +19,31 @@ class ControllerService extends BaseController implements ClassResourceInterface
         return $response;
     }
 
-    public function fetchCollection($entity, $alias, $leftJoinArray, $routeArray)
+    public function fetchCollection($entity, $alias, $routeArray, $leftJoinArray = null, $joinArray = null)
     {
         $request = $this->getRequest();
         $em = $this->getDoctrine()->getManager();
-        $queryBuilder = $em->createQueryBuilder()
+
+        $queryBuilder = $em->createQueryBuilder();
+
+//        $queryBuilder = new QueryBuilder();
+
+        $queryBuilder
             ->select($alias)
             ->from($entity, $alias);
-        foreach ($leftJoinArray as $leftJoin) {
-            $field = $leftJoin[0];
-            if (array_key_exists(1, $leftJoin)) {
-                $fieldAlias = $leftJoin[1];
-            }else{
-                $fieldAlias = $field;
+        if ($leftJoinArray !== null) {
+            foreach ($leftJoinArray as $leftJoin) {
+                $field = $leftJoin[0];
+                if (array_key_exists(1, $leftJoin)) {
+                    $fieldAlias = $leftJoin[1];
+                } else {
+                    $fieldAlias = $field;
+                }
+                $queryBuilder->leftJoin($alias . '.' . $field, $fieldAlias);
             }
-            $queryBuilder->leftJoin($alias . '.' . $field, $fieldAlias);
         }
+
+// todo join array here
 
         return $this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], true);
     }
@@ -46,14 +56,50 @@ class ControllerService extends BaseController implements ClassResourceInterface
      * @param array $routeArray
      * @return mixed
      */
-    public function fetchByOwningSide($owned, $alias, $owner, $id, $routeArray)
+    public function fetchByOwningSide($owned, $alias, $owner, $id, $routeArray, $leftJoinArray = null, $joinArray = null)
     {
         $request = $this->getRequest();
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->createQueryBuilder()
             ->select($alias)
-            ->from($owned, $alias)
-            ->where($alias . '.' . $owner . '= ?1')
+            ->from($owned, $alias);
+
+//        $queryBuilder = new QueryBuilder();
+
+        if ($leftJoinArray !== null) {
+            foreach ($leftJoinArray as $leftJoin) {
+                $field = $leftJoin[0];
+                if (array_key_exists(1, $leftJoin)) {
+                    $fieldAlias = $leftJoin[1];
+                } else {
+                    $fieldAlias = $field;
+                }
+                $queryBuilder->leftJoin($alias . '.' . $field, $fieldAlias);
+            }
+        }
+
+        if ($joinArray !== null) {
+            $field = null;
+            foreach ($joinArray as $join) {
+                $field = $join[0];
+                if (array_key_exists(1, $join)) {
+                    $fieldAlias = $join[1];
+                } else {
+                    $fieldAlias = $field;
+                }
+                if (array_key_exists(2, $join)) {
+                    $queryBuilder->join($field, $fieldAlias, 'WITH', $join[2]);
+                } else {
+                    $queryBuilder->join($field, $fieldAlias);
+                }
+            }
+            $queryBuilder->where($owner . '.id' . '= ?1');
+        } else {
+            $queryBuilder->where($alias . '.' . $owner . '= ?1');
+        }
+
+
+        $queryBuilder
             ->setParameter(1, $id);
         return ($this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], false));
     }
