7f7e674a878ee66463dd8826f2738dd9a5bfdbce
Lê Thanh Bình <binh@sunrise.vn>
2016-03-31 17:35:59 +0700

    TODO: - Map CandidateReviewer and CandidateFolder - Implement JobCandidateRepository - Handbook Content - ControllerService : prepareQBByOwningSide : bidirectional=false

--- Core/Framework/ControllerService.php
+++ Core/Framework/ControllerService.php
@@ -19,9 +19,17 @@ class ControllerService extends BaseController implements ClassResourceInterface
         return $response;
     }
 
-    public function fetchCollection($entity, $alias, $routeArray, $leftJoinArray = null, $joinArray = null, $conditionArray = null, $bidirectional = true)
+    /**
+     * @param $entity
+     * @param $alias
+     * @param null $leftJoinArray
+     * @param null $joinArray
+     * @param null $conditionArray
+     * @return QueryBuilder
+     */
+
+    public function prepareCollectionQB($entity, $alias, $leftJoinArray = null, $joinArray = null, $conditionArray = null)
     {
-        $request = $this->getRequest();
         $em = $this->getDoctrine()->getManager();
 
         $queryBuilder = $em->createQueryBuilder();
@@ -37,7 +45,7 @@ class ControllerService extends BaseController implements ClassResourceInterface
                 if (array_key_exists(1, $leftJoin)) {
                     $fieldAlias = $leftJoin[1];
                 } else {
-                    $fieldAlias = $field;
+                    $fieldAlias = explode('.', $field)[1];
                 }
                 $queryBuilder->leftJoin($alias . '.' . $field, $fieldAlias);
             }
@@ -50,7 +58,7 @@ class ControllerService extends BaseController implements ClassResourceInterface
                 if (array_key_exists(1, $join)) {
                     $fieldAlias = $join[1];
                 } else {
-                    $fieldAlias = $field;
+                    $fieldAlias = explode('.', $field)[1];
                 }
                 if (array_key_exists(2, $join)) {
                     $queryBuilder->join($field, $fieldAlias, 'WITH', $join[2]);
@@ -60,74 +68,80 @@ class ControllerService extends BaseController implements ClassResourceInterface
             }
         }
 
-        if (array_key_exists('where', $conditionArray)) {
-            $queryBuilder->where($conditionArray['where']);
-        }
-        if (array_key_exists('andWhere', $conditionArray)) {
-            $queryBuilder->where($conditionArray['andWhere']);
-        }
-        if (array_key_exists('parameters', $conditionArray)) {
-            $queryBuilder->setParameters($conditionArray['parameters']);
+        if ($conditionArray !== null) {
+            if (array_key_exists('where', $conditionArray)) {
+                $queryBuilder->where($conditionArray['where']);
+            }
+            if (array_key_exists('parameters', $conditionArray)) {
+                $queryBuilder->setParameters($conditionArray['parameters']);
+            }
+            if (array_key_exists('enabled', $conditionArray)) {
+                $queryBuilder->andWhere($queryBuilder->expr()->eq($alias . '.enabled', ':trueValue'))->setParameter('trueValue', true);
+            }
+            if (array_key_exists('not-system', $conditionArray)) {
+                $queryBuilder->andWhere($queryBuilder->expr()->eq($alias . '.system', ':falseValue'))->setParameter('falseValue', false);
+            }
         }
 
-
-        return $this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], true);
+        return $queryBuilder;
+//        return $this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], true);
     }
 
-    /**
-     * @param string $owned
-     * @param string $alias
-     * @param string $owner
-     * @param int $id
-     * @param array $routeArray
-     * @return mixed
-     */
-    public function fetchByOwningSide($owned, $alias, $owner, $id, $routeArray, $leftJoinArray = null, $joinArray = null, $conditionArray = null, $bidirectional = true)
+
+    public function prepareQBByOwningSide($owned, $ownedAlias, $owner, $ownerAlias, $ownerId, $leftJoinArray = null, $joinArray = null, $conditionArray = null, $bidirectional = true)
     {
-        $request = $this->getRequest();
-        $em = $this->getDoctrine()->getManager();
-        $queryBuilder = $em->createQueryBuilder()
-            ->select($alias)
-            ->from($owned, $alias);
+        if ($bidirectional) {
+            $joinArray[][] = $ownedAlias . '.' . $ownerAlias;
+            $qB = $this->prepareCollectionQB($owned, $ownedAlias, $leftJoinArray, $joinArray, $conditionArray);
+            $qB->where($qB->expr()->eq($ownerAlias . '.id', ':id'))->setParameter('id', $ownerId);
+            return $qB;
+        } else {
+
+        }
+//        $request = $this->getRequest();
+//        $em = $this->getDoctrine()->getManager();
+//        $queryBuilder = $em->createQueryBuilder()
+//            ->select($ownedAlias)
+//            ->from($owned, $ownedAlias);
 
 //        $queryBuilder = new QueryBuilder();
+//
+//        if ($leftJoinArray !== null) {
+//            foreach ($leftJoinArray as $leftJoin) {
+//                $field = $leftJoin[0];
+//                if (array_key_exists(1, $leftJoin)) {
+//                    $fieldAlias = $leftJoin[1];
+//                } else {
+//                    $fieldAlias = $field;
+//                }
+//                $queryBuilder->leftJoin($ownedAlias . '.' . $field, $fieldAlias);
+//            }
+//        }
+//
+//        if ($joinArray !== null) {
+//            $field = null;
+//            foreach ($joinArray as $join) {
+//                $field = $join[0];
+//                if (array_key_exists(1, $join)) {
+//                    $fieldAlias = $join[1];
+//                } else {
+//                    $fieldAlias = $field;
+//                }
+//                if (array_key_exists(2, $join)) {
+//                    $queryBuilder->join($field, $fieldAlias, 'WITH', $join[2]);
+//                } else {
+//                    $queryBuilder->join($field, $fieldAlias);
+//                }
+//            }
+//            $queryBuilder->where($ownerAlias . '.id' . '= ?1');
+//        } else {
+//            $queryBuilder->where($ownedAlias . '.' . $ownerAlias . '= ?1');
+//        }
 
-        if ($leftJoinArray !== null) {
-            foreach ($leftJoinArray as $leftJoin) {
-                $field = $leftJoin[0];
-                if (array_key_exists(1, $leftJoin)) {
-                    $fieldAlias = $leftJoin[1];
-                } else {
-                    $fieldAlias = $field;
-                }
-                $queryBuilder->leftJoin($alias . '.' . $field, $fieldAlias);
-            }
-        }
 
-        if ($joinArray !== null) {
-            $field = null;
-            foreach ($joinArray as $join) {
-                $field = $join[0];
-                if (array_key_exists(1, $join)) {
-                    $fieldAlias = $join[1];
-                } else {
-                    $fieldAlias = $field;
-                }
-                if (array_key_exists(2, $join)) {
-                    $queryBuilder->join($field, $fieldAlias, 'WITH', $join[2]);
-                } else {
-                    $queryBuilder->join($field, $fieldAlias);
-                }
-            }
-            $queryBuilder->where($owner . '.id' . '= ?1');
-        } else {
-            $queryBuilder->where($alias . '.' . $owner . '= ?1');
-        }
-
-
-        $queryBuilder
-            ->setParameter(1, $id);
-        return ($this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], false));
+//        $queryBuilder
+//            ->setParameter(1, $id);
+//        return ($this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], false));
     }
 
 }
