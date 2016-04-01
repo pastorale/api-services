bbca7e433b76517ab81f4d50dffa125731395045
Lê Thanh Bình <binh@sunrise.vn>
2016-04-01 18:43:41 +0700

    TODO: - Map CandidateReviewer and CandidateFolder - Implement JobCandidateRepository - Handbook Content
      public function getClassesAction(Request $request, $organisationId, Position $position)
        {
            $positionId = $position->getId();
            // condition is set in sub-query but parameters are set in the primary only or Doctrine wont understand.
            // in this case enabled=> true means position.enabled = true
            $queryBuilder = $this->prepareQBByOwningSide('AppBundle:Core\Classification\Tag', 'employeeClasses', 'AppBundle:Organisation\Position', 'position', $positionId, null, null, ['sub-query' => ['enabled' => true, 'andWheres' => [(new Expr())->eq('employeeClasses2.enabled', ':trueValue')]], 'parameters' => [':trueValue' => true]], false);
    
    //        $x = $queryBuilder->getDQL();
    //        $y = $queryBuilder->getQuery()->getSQL();
            return ($this->handlePagination($request, $queryBuilder, 'get_organisation_position_tags', array('organisationId' => $organisationId, 'position' => $position->getId())));
        }

--- Core/Framework/ControllerService.php
+++ Core/Framework/ControllerService.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Services\Core\Framework;
 
+use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 
@@ -11,6 +12,40 @@ use Symfony\Component\HttpFoundation\Response;
 class ControllerService extends BaseController implements ClassResourceInterface
 {
 //    use ContainerConstructorTrait;
+    private function processConditions(QueryBuilder $queryBuilder, $conditionArray, $alias)
+    {
+        if ($conditionArray !== null) {
+            $autobind = true;
+            if (array_key_exists('no-autobinding', $conditionArray)) {
+                $autobind = false;
+            }
+            if (array_key_exists('where', $conditionArray)) {
+                $queryBuilder->where($conditionArray['where']);
+            }
+            if (array_key_exists('andWheres', $conditionArray)) {
+                $andWheres = $conditionArray['andWheres'];
+                foreach ($andWheres as $andWhere) {
+                    $queryBuilder->andWhere($andWhere);
+                }
+            }
+            if (array_key_exists('parameters', $conditionArray)) {
+                $queryBuilder->setParameters($conditionArray['parameters']);
+            }
+            if (array_key_exists('enabled', $conditionArray)) {
+                $queryBuilder->andWhere($queryBuilder->expr()->eq($alias . '.enabled', ':trueValue'));
+                if ($autobind) {
+                    $queryBuilder->setParameter('trueValue', true);
+                }
+            }
+            if (array_key_exists('not-system', $conditionArray)) {
+                $queryBuilder->andWhere($queryBuilder->expr()->eq($alias . '.system', ':falseValue'));
+                if ($autobind) {
+                    $queryBuilder->setParameter('falseValue', false);
+                }
+            }
+        }
+        return $queryBuilder;
+    }
 
     public function optionsAction()
     {
@@ -35,10 +70,15 @@ class ControllerService extends BaseController implements ClassResourceInterface
         $queryBuilder = $em->createQueryBuilder();
 
 //        $queryBuilder = new QueryBuilder();
-
+        if (is_array($alias)) {
+            $selectAlias = $alias[0];
+            $entityAlias = $alias[1];
+        } else {
+            $selectAlias = $entityAlias = $alias;
+        }
         $queryBuilder
-            ->select($alias)
-            ->from($entity, $alias);
+            ->select($selectAlias)
+            ->from($entity, $entityAlias);
         if ($leftJoinArray !== null) {
             foreach ($leftJoinArray as $leftJoin) {
                 $field = $leftJoin[0];
@@ -68,20 +108,7 @@ class ControllerService extends BaseController implements ClassResourceInterface
             }
         }
 
-        if ($conditionArray !== null) {
-            if (array_key_exists('where', $conditionArray)) {
-                $queryBuilder->where($conditionArray['where']);
-            }
-            if (array_key_exists('parameters', $conditionArray)) {
-                $queryBuilder->setParameters($conditionArray['parameters']);
-            }
-            if (array_key_exists('enabled', $conditionArray)) {
-                $queryBuilder->andWhere($queryBuilder->expr()->eq($alias . '.enabled', ':trueValue'))->setParameter('trueValue', true);
-            }
-            if (array_key_exists('not-system', $conditionArray)) {
-                $queryBuilder->andWhere($queryBuilder->expr()->eq($alias . '.system', ':falseValue'))->setParameter('falseValue', false);
-            }
-        }
+        $this->processConditions($queryBuilder, $conditionArray, $entityAlias);
 
         return $queryBuilder;
 //        return $this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], true);
@@ -94,54 +121,42 @@ class ControllerService extends BaseController implements ClassResourceInterface
             $joinArray[][] = $ownedAlias . '.' . $ownerAlias;
             $qB = $this->prepareCollectionQB($owned, $ownedAlias, $leftJoinArray, $joinArray, $conditionArray);
             $qB->where($qB->expr()->eq($ownerAlias . '.id', ':id'))->setParameter('id', $ownerId);
-            return $qB;
-        } else {
+        } else { // if the owned doesn't that he/she is owned by the owner.
+            if (array_key_exists('sub-query', $conditionArray)) {
+                $subQueryConditionArray = $conditionArray['sub-query'];
+            }
 
+            $subQueryConditionArray['where'] = (new Expr())->eq($ownerAlias, ':ownerId');
+            $ownedAlias2 = $ownedAlias . '2';
+            $ownedAlias2_id = $ownedAlias2 . '.id';
+            $ownedAlias_id = $ownedAlias . '.id';
+            $subQB = $this->prepareCollectionQB($owner, [$ownedAlias2_id, $ownerAlias], null, [[$ownerAlias . '.' . $ownedAlias, $ownedAlias2]], $subQueryConditionArray);
+//            $x = $subQB->getDQL();
+//            $y = $subQB->getQuery()->getSQL();
+            $qB = $this->prepareCollectionQB($owned, $ownedAlias, $leftJoinArray, $joinArray, $conditionArray);
+            $qB->andWhere($qB->expr()->in($ownedAlias_id, $subQB->getDQL()));
+            // parameters key does not exist it will be created automatically by PHP
+            $conditionArray['parameters']['ownerId'] = $ownerId;
+            // process the conditions at the top-most level
+            $this->processConditions($qB, $conditionArray, $ownedAlias);
+
+            /**
+             * $qBPos = $em->createQueryBuilder();
+             * $qBPos->select('tag2.id')->from('AppBundle:Organisation\Position', 'position')
+             * ->join('position.employeeClasses', 'tag2')->andWhere($qBPos->expr()->eq('position', '?1'))
+             * ->andWhere($qBPos->expr()->eq('tag2.enabled', '?2'))
+             * ->andWhere($qBPos->expr()->eq('tag2.system', '?3'));
+             *
+             *
+             * $queryBuilder = $em->createQueryBuilder();
+             * $queryBuilder->select('tag')->from('AppBundle:Core\Classification\Tag', 'tag')
+             * //            ->join('AppBundle:Organisation\Business\Business', 'business', 'WITH', '1=1')
+             * //            ->join('business.tags', 'tags', 'WITH', 'tags.id = tag.id')
+             * ;
+             * $queryBuilder->where($queryBuilder->expr()->in('tag.id', $qBPos->getDQL()));
+             * $queryBuilder->setParameters([1 => $position->getId(), 2 => true, 3 => false]);
+             */
         }
-//        $request = $this->getRequest();
-//        $em = $this->getDoctrine()->getManager();
-//        $queryBuilder = $em->createQueryBuilder()
-//            ->select($ownedAlias)
-//            ->from($owned, $ownedAlias);
-
-//        $queryBuilder = new QueryBuilder();
-//
-//        if ($leftJoinArray !== null) {
-//            foreach ($leftJoinArray as $leftJoin) {
-//                $field = $leftJoin[0];
-//                if (array_key_exists(1, $leftJoin)) {
-//                    $fieldAlias = $leftJoin[1];
-//                } else {
-//                    $fieldAlias = $field;
-//                }
-//                $queryBuilder->leftJoin($ownedAlias . '.' . $field, $fieldAlias);
-//            }
-//        }
-//
-//        if ($joinArray !== null) {
-//            $field = null;
-//            foreach ($joinArray as $join) {
-//                $field = $join[0];
-//                if (array_key_exists(1, $join)) {
-//                    $fieldAlias = $join[1];
-//                } else {
-//                    $fieldAlias = $field;
-//                }
-//                if (array_key_exists(2, $join)) {
-//                    $queryBuilder->join($field, $fieldAlias, 'WITH', $join[2]);
-//                } else {
-//                    $queryBuilder->join($field, $fieldAlias);
-//                }
-//            }
-//            $queryBuilder->where($ownerAlias . '.id' . '= ?1');
-//        } else {
-//            $queryBuilder->where($ownedAlias . '.' . $ownerAlias . '= ?1');
-//        }
-
-
-//        $queryBuilder
-//            ->setParameter(1, $id);
-//        return ($this->handlePagination($request, $queryBuilder, $routeArray[0], $routeArray[1], false));
+        return $qB;
     }
-
 }
