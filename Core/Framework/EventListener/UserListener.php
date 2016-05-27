b4302c1e98d23236360ac0c9f356d99ee477b095
mikedutuandu <mikedutuandu@gmail.com>
2016-05-27 17:41:57 +0700

    update code

--- Core/Framework/EventListener/UserListener.php
+++ Core/Framework/EventListener/UserListener.php
@@ -6,6 +6,12 @@ use AppBundle\Entity\Core\User\User;
 
 class UserListener
 {
+    private $container;
+    public function setContainer($container)
+    {
+        $this->container = $container;
+    }
+
     public function prePersist(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
@@ -13,10 +19,13 @@ class UserListener
         if (!$entity instanceof User) {
             return;
         }
-
+        $userManager = $this->container->get('fos_user.user_manager');
         $entityManager = $args->getEntityManager();
         $entity->setUsername($entity->getEmail());
         $entity->setUsernameCanonical($entity->getEmail());
-
+        $entity->setEmailCanonical($entity->getEmail());
+        $entity->setPlainPassword(uniqid());
+        $entity->setCode(uniqid());
+        $userManager->updatePassword($entity);
     }
 }
