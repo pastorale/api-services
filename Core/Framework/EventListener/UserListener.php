ad22fe9b54bde7e67b2e4b8a1e93e7acc9a0b106
mikedutuandu <mikedutuandu@gmail.com>
2016-05-27 12:10:41 +0700

    update code

---
+++ Core/Framework/EventListener/UserListener.php
@@ -0,0 +1,21 @@
+<?php
+namespace AppBundle\Services\Core\Framework\EventListener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use AppBundle\Entity\Core\User\User;
+
+class UserListener
+{
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if (!$entity instanceof User) {
+            return;
+        }
+
+        $entityManager = $args->getEntityManager();
+        $entity->setUsername($entity->getEmail());
+
+    }
+}
