04a9503cef347ddb4be1a129d5df371b4753f46a
mikedu <mikedutuandu@gmail.com>
2016-09-14 00:00:11 +0700

    update code

--- Core/Message/EmailMessenger.php
+++ Core/Message/EmailMessenger.php
@@ -3,6 +3,7 @@ namespace AppBundle\Services\Core\Message;
 
 use AppBundle\Entity\Core\Message\Message;
 use AppBundle\Entity\Core\Message\MessageTemplate;
+use AppBundle\Entity\Organisation\Organisation;
 use AppBundle\Services\Core\Framework\BaseController;
 
 class EmailMessenger extends BaseController
@@ -18,12 +19,14 @@ class EmailMessenger extends BaseController
         return $message;
     }
 
-    public function sendMessage(Message $message)
+    public function sendMessage(Message $message,$from=null,$to=null)
     {
+        $from = $from === null ? $message->getSender()->getEmail():$from;
+        $to = $to === null ? $message->getRecipient()->getEmail():$to;
         $email = \Swift_Message::newInstance()
             ->setSubject($message->getSubject())
-            ->setFrom($message->getSender())
-            ->setTo($message->getRecipient())
+            ->setFrom($from)
+            ->setTo($to)
             ->setContentType("text/html")
             ->setBody($message->getBody());
 
@@ -34,5 +37,24 @@ class EmailMessenger extends BaseController
         $spool = $mailer->getTransport()->getSpool();
         $transport = $this->container->get('swiftmailer.transport.real');
         $spool->flushQueue($transport);
+    }
+    public function sendInformationLoginCloudbook(Organisation $organisation,$data){
+        $type = $data['type'];
+        $users =$data['users'];
+        $emailTemplate = $this->getDoctrine()->getRepository('AppBundle:Core\Message\MessageTemplate')->findOneByCode($type);
+        foreach ($users as $user){
+            $vars['COMPANY_NAME'] = $organisation->getName();
+            $vars['FULL_NAME'] = $user['full_name'];
+            $vars['WEB_USERNAME']=$user['web_username'];
+            $vars['WEB_PASSWORD']='p@ssword';
+            $vars['APP_USERNAME']=$organisation->getCode();
+            $vars['APP_PASSWORD']=$user['app_password'];
+
+            $message = new Message();
+            $this->prepareMessageContent($message,$emailTemplate,$vars);
+            $this->sendMessage($message,'magenta@magenta.com',$user['email']);
+
+        }
+
     }
 }
